e15867a20a8c17ddb7fc2ad2848ef4e58b65e954
mjpodias <mikepodias@gmail.com>
2016-03-12 19:09:48 -0500

    Initial commit.

---
+++ src/Bin.java
@@ -0,0 +1,141 @@
+public class Bin {
+	private int binNumber;
+	private String trialNumber;
+	private String actorID;
+	private String centerDur;
+	private String stayHideDur;
+	private String socialContactDur;
+	private String threeContactDur;
+	private String fiveContactDur;
+	private String sevenContactDur;
+	private String oneContactDur; 
+	private String twoContactDur;
+	private String fourContactDur;
+	private String sixContactDur;
+	private String approachDur;
+	private String leaveDur;
+	private String followDur;
+	private String sniffDur;
+	private String mountDur;
+	private String competeDur;
+
+	public Bin(int binNumber, String trialNumber, String actorID) {
+		this.binNumber = binNumber;
+		this.trialNumber = trialNumber;
+		this.actorID = actorID;
+	}
+	public int getBinNumber() {
+		return binNumber;
+	}
+	public void setBinNumber(int binNumber) {
+		this.binNumber = binNumber;
+	}
+	public String getTrialNumber() {
+		return trialNumber;
+	}
+	public void setTrialNumber(String trialNumber) {
+		this.trialNumber = trialNumber;
+	}
+	public String getActorID() {
+		return actorID;
+	}
+	public void setActorID(String actorID) {
+		this.actorID = actorID;
+	}
+	public String getCenterDur() {
+		return centerDur;
+	}
+	public void setCenterDur(String centerDur) {
+		this.centerDur = centerDur;
+	}
+	public String getStayHideDur() {
+		return stayHideDur;
+	}
+	public void setStayHideDur(String stayHideDur) {
+		this.stayHideDur = stayHideDur;
+	}
+	public String getSocialContactDur() {
+		return socialContactDur;
+	}
+	public void setSocialContactDur(String socialContactDur) {
+		this.socialContactDur = socialContactDur;
+	}
+	public String getThreeContactDur() {
+		return threeContactDur;
+	}
+	public void setThreeContactDur(String threeContactDur) {
+		this.threeContactDur = threeContactDur;
+	}
+	public String getFiveContactDur() {
+		return fiveContactDur;
+	}
+	public void setFiveContactDur(String fiveContactDur) {
+		this.fiveContactDur = fiveContactDur;
+	}
+	public String getSevenContactDur() {
+		return sevenContactDur;
+	}
+	public void setSevenContactDur(String sevenContactDur) {
+		this.sevenContactDur = sevenContactDur;
+	}
+	public String getOneContactDur() {
+		return oneContactDur;
+	}
+	public void setOneContactDur(String oneContactDur) {
+		this.oneContactDur = oneContactDur;
+	}
+	public String getTwoContactDur() {
+		return twoContactDur;
+	}
+	public void setTwoContactDur(String twoContactDur) {
+		this.twoContactDur = twoContactDur;
+	}
+	public String getFourContactDur() {
+		return fourContactDur;
+	}
+	public void setFourContactDur(String fourContactDur) {
+		this.fourContactDur = fourContactDur;
+	}
+	public String getSixContactDur() {
+		return sixContactDur;
+	}
+	public void setSixContactDur(String sixContactDur) {
+		this.sixContactDur = sixContactDur;
+	}
+	public String getApproachDur() {
+		return approachDur;
+	}
+	public void setApproachDur(String approachDur) {
+		this.approachDur = approachDur;
+	}
+	public String getLeaveDur() {
+		return leaveDur;
+	}
+	public void setLeaveDur(String leaveDur) {
+		this.leaveDur = leaveDur;
+	}
+	public String getFollowDur() {
+		return followDur;
+	}
+	public void setFollowDur(String followDur) {
+		this.followDur = followDur;
+	}
+	public String getSniffDur() {
+		return sniffDur;
+	}
+	public void setSniffDur(String sniffDur) {
+		this.sniffDur = sniffDur;
+	}
+	public String getMountDur() {
+		return mountDur;
+	}
+	public void setMountDur(String mountDur) {
+		this.mountDur = mountDur;
+	}
+	public String getCompeteDur() {
+		return competeDur;
+	}
+	public void setCompeteDur(String competeDur) {
+		this.competeDur = competeDur;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,155 @@
+import java.io.*;
+import com.opencsv.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		CSVReader reader = new CSVReader(new FileReader("Bin.csv"));
+		String [] nextLine;
+		int binnumber = 1;
+		HashMap<String, Bin> hm = new HashMap<String, Bin>();
+		
+		while ((nextLine = reader.readNext()) != null) {
+	        // nextLine[] is an array of values from the line  
+			String binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+			
+			if (!hm.containsKey(binKey)){
+				for (binnumber=1; binnumber <= 120; binnumber++){
+					binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+					hm.put(binKey, new Bin(binnumber, nextLine[0], nextLine[1]));
+				}
+				binnumber=1;
+			}
+			
+			if (hm.containsKey(binKey)){
+				switch (nextLine[2]) {
+					case "Center in Cage": 
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setCenterDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Stay/Hide in Cage": 
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setStayHideDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Contact": 
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setSocialContactDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Contact <0.030": 
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setThreeContactDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Contact <0.050":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setFiveContactDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Contact <0.070":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setSevenContactDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Contact <0.100":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setOneContactDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Contact <0.200":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setTwoContactDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Contact <0.400":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setFourContactDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Contact <0.600":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setSixContactDur(nextLine[binnumber+2]);
+							System.out.println("Something is working");
+						}
+						binnumber=1;
+						break;
+					case "Social Approach":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setApproachDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Leave":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setLeaveDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Follow":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setFollowDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Sniff":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setSniffDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Mount":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setMountDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					case "Social Compete":  
+						for (binnumber=1; binnumber <= 120; binnumber++){
+							binKey = nextLine[0] + nextLine[1] + String.valueOf(binnumber);
+							hm.get(binKey).setCompeteDur(nextLine[binnumber+2]);
+						}
+						binnumber=1;
+						break;
+					default: 
+						break;
+				}
+			}	
+	    }
+		
+		int counter = 1;
+		
+//		for (String binKey : hm.keySet()) {
+//		    System.out.println(binKey + " | " + hm.get(binKey).getSixContactDur() + ":::enter number " + counter);
+//		    counter++;
+//		}
+		reader.close();
+		
+	}
+}
